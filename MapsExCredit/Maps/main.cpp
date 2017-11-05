1cd14c17bc34e25aa2a54e122dfc3f6baa7e9c01
Matt <cclinard84@gmail.com>
2017-11-05 14:44:39 -0500

    feat: Finished

--- MapsExCredit/Maps/main.cpp
+++ MapsExCredit/Maps/main.cpp
@@ -48,45 +48,23 @@ bool operator<(const Student s1, const Student s2){
 }
 
 bool compareStudents(std::pair<string,Student> , std::pair<string,Student>);
+void createStudents(Student[]);
+void buildMap(map<string, Student>&, Student[]);
 
 int main(int argc, char **argv)
 {
     map<string, Student> mymap;
-   
-    
     Student students[10];
-    students[0] = Student( "Lovelace", "Ada", "P20112845", 4.0 );
-    students[1] = Student( "Von Neumann", "John", "P20112844", 3.9 );
-    students[2] = Student( "Torvald", "Linus", "P20112843", 3.8 );
-    students[3] = Student( "Turing", "Alan", "P20112842", 3.7 );
-    students[4] = Student( "Hamilton", "Margaret", "P20112841", 3.6 );
-    students[5] = Student( "Knuth", "Donald", "P20112840", 3.5 );
-    students[6] = Student( "Bloom", "Burton", "P20112839", 3.4 );
-    students[7] = Student( "Hooper", "Grace", "P20112838", 3.2 );
-    students[8] = Student( "Holberton", "Betty", "P20112837", 3.1 );
-    students[9] = Student( "Sammet", "Jean", "P20112836", 3.0 );
-    
-    mymap.insert( std::pair<string,Student>( students[5].id, students[5]) );
-    mymap.insert( std::pair<string,Student>( students[4].id, students[4]) );
-    mymap.insert( std::pair<string,Student>( students[3].id, students[3]) );
-    mymap.insert( std::pair<string,Student>( students[2].id, students[2]) );
-    mymap.insert( std::pair<string,Student>( students[1].id, students[1]) );
-    mymap.insert( std::pair<string,Student>( students[0].id, students[0]) );
-    mymap.insert( std::pair<string,Student>( students[6].id, students[6]) );
-    mymap.insert( std::pair<string,Student>( students[7].id, students[7]) );
-    mymap.insert( std::pair<string,Student>( students[8].id, students[8]) );
-    mymap.insert( std::pair<string,Student>( students[9].id, students[9]) );
-    
+    map<string, Student>::const_iterator cIt;   
     string userInput = "-1";
-    map<string, Student>::reverse_iterator it = mymap.rbegin();
-    
-   
+
 	cout << "Hello! Welcome to student mapper!" << endl;
+    createStudents(students);
+    buildMap(mymap, students);
     cout << "The smallest element is " << std::min_element(mymap.begin(),mymap.end(), compareStudents)->second << endl;
     cout << "Now printing reverse order by student id:" << endl;
-     while(it != mymap.rend()){
-        cout << it->second << endl;
-        it++;
+   for(auto cIt = mymap.crbegin(); cIt != mymap.crend(); ++cIt){
+        cout << (*cIt).second << endl;
     }
     cout << "Input the student ID of the student you want to search for or 0 to exit: ";
     cin >> userInput;
@@ -110,3 +88,27 @@ bool compareStudents(std::pair<string,Student> pair1, std::pair<string,Student> 
     return pair1.second.lastName < pair2.second.lastName;
 }
 
+void createStudents(Student students[10]){
+    students[0] = Student( "Lovelace", "Ada", "P20112845", 4.0 );
+    students[1] = Student( "Von Neumann", "John", "P20112844", 3.9 );
+    students[2] = Student( "Torvald", "Linus", "P20112843", 3.8 );
+    students[3] = Student( "Turing", "Alan", "P20112842", 3.7 );
+    students[4] = Student( "Hamilton", "Margaret", "P20112841", 3.6 );
+    students[5] = Student( "Knuth", "Donald", "P20112840", 3.5 );
+    students[6] = Student( "Bloom", "Burton", "P20112839", 3.4 );
+    students[7] = Student( "Hooper", "Grace", "P20112838", 3.2 );
+    students[8] = Student( "Holberton", "Betty", "P20112837", 3.1 );
+    students[9] = Student( "Sammet", "Jean", "P20112836", 3.0 );
+}
+void buildMap(map<string, Student> &mymap, Student students[10]){
+    mymap.insert( std::pair<string,Student>( students[5].id, students[5]) );
+    mymap.insert( std::pair<string,Student>( students[4].id, students[4]) );
+    mymap.insert( std::pair<string,Student>( students[3].id, students[3]) );
+    mymap.insert( std::pair<string,Student>( students[2].id, students[2]) );
+    mymap.insert( std::pair<string,Student>( students[1].id, students[1]) );
+    mymap.insert( std::pair<string,Student>( students[0].id, students[0]) );
+    mymap.insert( std::pair<string,Student>( students[6].id, students[6]) );
+    mymap.insert( std::pair<string,Student>( students[7].id, students[7]) );
+    mymap.insert( std::pair<string,Student>( students[8].id, students[8]) );
+    mymap.insert( std::pair<string,Student>( students[9].id, students[9]) );
+}
